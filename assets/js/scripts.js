4fe2288f078cac35c4466446c5784d6ee0c99556
Paulo Magalhães <paulomaga225@gmail.com>
2022-06-18 12:07:26 -0300

    Implementando os eventos usando o método addEventListener

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,12 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
+var adicionar = document.getElementById('adicionar');
+var subtrair = document.getElementById('subtrair');
+
+adicionar.addEventListener('click', increment);
+subtrair.addEventListener('click', decrement);
+
 function increment() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
